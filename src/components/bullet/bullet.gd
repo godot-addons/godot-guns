5417b3b5458a8b70d868321abc4a5b03f962d85c
Brandon Lamb <brandon@brandonlamb.com>
2017-08-11 16:50:14 -0700

    cant quite figure out why tracking doesnt work

--- src/components/bullet/bullet.gd
+++ src/components/bullet/bullet.gd
@@ -6,13 +6,13 @@ const SPRITE_NODE_NAME = "Sprite"
 const COLLIDER_NODE_NAME = "collision"
 
 # misc vars
-export var fire_pos_offset = [0.0,0.0]
-export var follow_gun = false
-export var fit_collider_to_sprite = true setget set_fit_collider_to_sprite
+export var fire_pos_offset = [0.0, 0.0]
+export(bool) var follow_gun = false
+export(bool) var fit_collider_to_sprite = true setget set_fit_collider_to_sprite
 
 # scaling change related vars
-export var size_scaling_velocity = [0.0,0.0]
-export var max_size_scale = [0.0,0.0]
+export var size_scaling_velocity = [0.0, 0.0]
+export var max_size_scale = [0.0, 0.0]
 
 # death/kill/free() related vars
 export(bool) var kill_on_collide = false setget set_kill_on_collide
@@ -31,9 +31,12 @@ var _I = 0
 var _D = 0
 
 # PID controller gain values
-var _PID_Kp = 1000.0
-var _PID_Ki = 100.0
-var _PID_Kd = 1000.0
+#var _PID_Kp = 1000.0
+#var _PID_Ki = 100.0
+#var _PID_Kd = 1000.0
+var _PID_Kp = 2000.0
+var _PID_Ki = 50.0
+var _PID_Kd = 100.0
 
 var _traveled_dist = 0
 var _prev_pos = null
@@ -62,6 +65,10 @@ func _fixed_process(delta):
 		else:
 			_prev_pos = global_position
 
+func _integrate_forces(state):
+	if size_scaling_velocity[0] != 0 and size_scaling_velocity[1] != 0:
+		_scale_bullet()
+
 func setup(shooting_gun):
 	gun_shot_from = shooting_gun
 	set_z(min(get_z(), gun_shot_from.get_z() - 1)) #ensure behind gun
@@ -89,13 +96,13 @@ func setup(shooting_gun):
 
 	#set_global_rot(gun_shot_from.get_global_rot())
 	global_rotation = gun_shot_from.global_rotation
-	
+
 	#_set_vel_from_angle(get_global_rot())
 	_set_vel_from_angle(global_rotation)
 
 func _set_vel_from_angle(angle):
 	#magnitude of rigid body's linear velocity
-	var speed = sqrt(get_linear_velocity().length_squared()) 
+	var speed = sqrt(get_linear_velocity().length_squared())
 	#var vx = speed * cos(-angle) #idk why this is negative?
 	var vx = speed * cos(angle)
 	var vy = speed * sin(angle)
@@ -118,22 +125,20 @@ func _scale_bullet():
 	#set_scale(Vector2(new_x, new_y))
 	scale = Vector2(new_x, new_y)
 
-func _integrate_forces(state):
-	if size_scaling_velocity[0] != 0 and size_scaling_velocity[1] != 0:
-		_scale_bullet()
-
-func _get_PID_output(currentError, delta):
-	_P = currentError
+func _get_PID_output(current_error, delta):
+	_P = current_error
 	_I += _P * delta
 	_D = (_P - _prev_error) / delta
-	_prev_error = currentError
+	_prev_error = current_error
 
 	return _P * _PID_Kp + _I * _PID_Ki + _D * _PID_Kd
 
+# Something in this function needs some fixing
 func _track_target(delta):
-	var angle_btw = global_position.angle_to_point(target.global_position) + PI / 2
-	#var error = global_rotation_deg - angle_btw
+	var angle_btw = global_position.angle_to_point(target.global_position) - PI / 2
 	var error = global_rotation - angle_btw
+	#error = rad2deg(error)
+
 	#deal with angle discontinuity
 	#https://stackoverflow.com/questions/10697844/how-to-deal-with-the-discontinuity-of-yaw-angle-at-180-degree
 	if error > PI:
@@ -141,9 +146,13 @@ func _track_target(delta):
 	elif error < -PI:
 		 error = error + PI * 2
 
+	#if error >= 180:
+	#	error = error - 360 * delta
+	#elif error <= -180:
+	#	error = error + 360 * delta
+
 	var torque = _get_PID_output(error, delta)
 	set_applied_torque(torque)
-	#_set_vel_from_angle(global_rotation_deg)
 	_set_vel_from_angle(global_rotation)
 
 func set_fit_collider_to_sprite(val):
@@ -153,11 +162,13 @@ func set_fit_collider_to_sprite(val):
 
 func set_target(target_node, PID_Kp = -1, PID_Ki = -1, PID_Kd = -1):
 	target = target_node
-	if (PID_Kp >= 0):
+	if PID_Kp >= 0:
 		_PID_Kp = PID_Kp
-	if (PID_Ki >= 0):
+
+	if PID_Ki >= 0:
 		_PID_Ki = PID_Ki
-	if (PID_Kd >= 0):
+
+	if PID_Kd >= 0:
 		_PID_Kd = PID_Kd
 
 func set_kill_after_time(val):
