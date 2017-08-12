7c0c3eb01125871461e74a473e09b34befe52b4a
Brandon Lamb <brandon@brandonlamb.com>
2017-08-12 04:44:39 -0700

    add small bullet

--- src/scenes/tracker.gd
+++ src/scenes/tracker.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var enemy_ship = $"../enemy_ship"
 
 func _ready():
-	$weapon.connect("volley_fired", self, "set_targets")
+	$weapon_1.connect("volley_fired", self, "set_targets")
 
 func set_targets(bullets):
 	for bullet in bullets:
